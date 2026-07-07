caa6d98c7600a120ab990caa59438d36204bc59c
Marco Widesott <marco.widesott@anitia.it>
2026-07-07 16:51:18 +0200

    feat: init login

--- 02-auth-jwt/backend/main.go
+++ 02-auth-jwt/backend/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Message string `json:"message"`
+}
+
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", health)
+	mux.HandleFunc("GET /health", health)
+	mux.HandleFunc("POST /login", login)
 	log.Println("Server running on :8080")
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
@@ -20,4 +31,19 @@ func main() {
 // e i dati della richiesta in arrivo (r).
 func health(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Ok")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var req LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	response := LoginResponse{
+		Message: "Login successful",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
