055289c697e6e2967ed3a8052eac7e69441b600f
Marco Widesott <marco.widesott@anitia.it>
2026-07-16 18:08:31 +0200

    feat: added createService

--- 10-uptime-monitor/backend/repository.go
+++ 10-uptime-monitor/backend/repository.go
@@ -1,10 +1,13 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 func listServices(db *sql.DB) ([]Service, error) {
 	rows, err := db.Query(`
-		SELECT id, name, url,interval_seconds, created_at
+		SELECT id, name, url, interval_seconds, created_at
 		FROM services
 		ORDER BY id DESC
 	`)
@@ -43,4 +46,34 @@ func listServices(db *sql.DB) ([]Service, error) {
 
 	return services, nil
 
+}
+
+func createService(db *sql.DB, req ServiceRequest) (Service, error) {
+
+	now := time.Now().Format(time.RFC3339)
+
+	result, err := db.Exec(`
+		INSERT INTO services (name, url, interval_seconds, created_at)
+		VALUES (?, ?, ?, ?)
+		`, req.Name, req.URL, req.IntervalSeconds, now)
+	//quanto sopra sono i parametri che passo agli ?
+	if err != nil {
+		return Service{}, err
+	}
+
+	//LastInsertId mi restituisce il valore dell' id messo in tabella
+	id, err := result.LastInsertId()
+	if err != nil {
+		return Service{}, err
+	}
+
+	service := Service{
+		ID:              int(id),
+		Name:            req.Name,
+		URL:             req.URL,
+		IntervalSeconds: req.IntervalSeconds,
+		CreatedAt:       now,
+	}
+
+	return service, nil
 }
